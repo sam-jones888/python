c4d480c5a7ed390d66458577c7f809b1d48f8938
Sam Jones <134706572+sam-jones888@users.noreply.github.com>
2023-07-01 15:20:18 +0300

    math_module

---
+++ tanh.py
@@ -0,0 +1,5 @@
+import math                 #math.tanh(x) == (exp(x) - exp(-x)) / (exp(x) + exp(-x))
+                            #math.exp(x) == e
+x = 2.5                     #e == 2.7182
+tanh_x = math.tanh(x)
+print(tanh_x)
