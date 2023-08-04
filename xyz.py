4b7c21bcb29028e6ab3f6a968d8bb5a99d4af068
Sam Jones <134706572+sam-jones888@users.noreply.github.com>
2023-08-04 22:11:29 +0300

    xyz.py

--- xyz.py
+++ xyz.py
@@ -2,10 +2,12 @@ def func(x,y):
   a = 0
   b = 0
   z = a + b
+  a += x
+  b += y
   if z == 0:
     z ** 2
   return z
 x = int(input())
 y = int(input())
-a.append(x)
-b.append(y)
+func(x,y)
+print(x,y)
